d325223c452025dedbb6a19cac4a94e44e68db64
Sebastian Penhouet <s.penhouet@outlook.de>
2025-03-14 01:03:30 +0100

    Make output path configurable

--- confluence_to_markdown/main.py
+++ confluence_to_markdown/main.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import Annotated
+
 import typer
 
 from confluence_to_markdown.confluence import Page
@@ -5,23 +8,28 @@ from confluence_to_markdown.confluence import Page
 app = typer.Typer()
 
 
-# TODO make output path configurable
 # TODO build and publish to pypi
 
 
 @app.command()
-def page(page_id: int) -> None:
+def page(
+    page_id: Annotated[int, typer.Argument()],
+    output_path: Annotated[Path, typer.Argument()] = Path("."),
+) -> None:
     page = Page.from_id(page_id)
 
-    with open(f"scratch/{page.title}.html", "w") as file:
+    with open(output_path / f"{page.title}.html", "w") as file:
         file.write(page.html)
 
-    with open(f"scratch/{page.title}.md", "w") as file:
+    with open(output_path / f"{page.title}.md", "w") as file:
         file.write(page.markdown)
 
 
 @app.command()
-def space(space_key: str) -> None:
+def space(
+    space_key: Annotated[str, typer.Argument()],
+    output_path: Annotated[Path, typer.Argument()] = Path("."),
+) -> None:
     pass
     # api_session = get_session()
     # space = api_session.get_space(space_key)
